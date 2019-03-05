ef9163b666a5bc80347ee71a4c17e51ef8f9b137
Martin Chan <martinchan53@gmail.com>
2019-03-05 18:11:38 +0000

    Update documentation

--- R/as_percent.R
+++ R/as_percent.R
@@ -2,6 +2,7 @@
 #' Convert a numeric value into a string with percentage sign.
 #' @param num Numeric vector to pass through
 #' @param rounding Number of decimal places to round to. Default is 0.
+#' @export
 as_percent <- function(num, rounding = 0){
   paste0(round(num * 100, rounding),"%")
 }
