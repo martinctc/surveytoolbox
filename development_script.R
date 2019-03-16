4994942e14c8035f909735d1532b052ab7e89ab4
Martin Chan <martinchan53@gmail.com>
2019-03-16 23:25:43 +0000

    Update development_script.R

--- development_script.R
+++ development_script.R
@@ -1,18 +1,28 @@
 # install.packages(c("devtools", "roxygen2", "testthat", "knitr"))
 # install.packages("rstudioapi")
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
 rstudioapi::isAvailable("0.99.149")
 
 #' Load these packages in first
-library(tidyverse) 
+library(tidyverse)
 library(devtools)
 library(roxygen2)
 
 
 roxygen2::roxygenise()
 #' Run these three functions to build up the package
-build()
-install()
-document()
+devtools::build()
+devtools::install()
+devtools::document()
 
 devtools::build_manual()
 devtools::check(manual=TRUE) # check whether everything is there
