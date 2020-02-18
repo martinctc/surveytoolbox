266df5a5805eb28c77ac4a2bc53eef3ec1c9bc9b
Martin Chan <martin.chan@Rainmakers.local>
2020-02-18 13:48:41 +0000

    Add examples to documentation
    Added for apply_row. Also allow custom values for as_nps_cat

--- R/apply_row.R
+++ R/apply_row.R
@@ -12,12 +12,16 @@
 #' library(tidyverse)
 #' iris %>% mutate(Sepal_Sum = apply_row(.,select_helpers = contains("Sepal"), sum, na.rm = TRUE))
 #' iris %>% mutate(Sepal_Sum = apply_row(.,c("Sepal.Length","Petal.Width"), function(x) sum(x > 1))) 
+#' 
+#' Can use a character vector to select columns
+#' petal_str <- c("Petal.Length", "Petal.Width")
+#' iris %>% mutate(Any_Petal = apply_row(., petal_str, function(x) any(x > 1)))
 #' }
-
+#' 
 #' 
 #' @export
 
 apply_row <- function(x, select_helpers = everything(), FUN, ...){
-  apply(select(x,select_helpers),1, FUN, ...)
+  apply(select(x, select_helpers), 1, FUN, ...)
 }
 
--- R/as_nps_cat.R
+++ R/as_nps_cat.R
@@ -1,22 +1,29 @@
 #' Convert numeric variable to NPS categorical variable
 #' 
-#' Returns a categorical variable with values 1, 2, and 3.
+#' Returns a categorical variable with default values over 1, 2, and 3.
 #' Suited for running multinomial logistic regression. 
 #' To calculate the NPS score, use `as_nps()`.
 #' 
 #' @param x Numeric variable to pass through. Valid range is 0 to 10 inclusive, otherwise returns a NA.
+#' @param det Numeric value to represent the code for Detractor. Defaults to 1.
+#' @param pas Numeric value to represent the code for Passive. Defaults to 2.
+#' @param pro Numeric value to represent the code for Promoter. Defaults to 3.
 #' 
 #' @export
-as_nps_cat <-function(x){
-  dplyr::case_when(x >=0 & x<=6 ~ 1,
-                   x >=7 & x<=8 ~ 2,
-                   x >=9 & x<=10 ~ 3,
-                   TRUE ~ NA_real_)-> out_x
-  
-  labz <- c("Detractor" = 1,
-            "Passive" = 2,
-            "Promoter" = 3,
-            "Missing value" = NA_real_)
-  
-  haven::labelled(out_x,labz)
+as_nps_cat <-function(x, det = 1, pas = 2, pro = 3){
+  if(any(!is.numeric(c(det, pas, pro)))){
+    stop("Values must be numeric.")
+  } else {
+    dplyr::case_when(x >=0 & x<=6 ~ det,
+                     x >=7 & x<=8 ~ pas,
+                     x >=9 & x<=10 ~ pro,
+                     TRUE ~ NA_real_)-> out_x
+    
+    labz <- c("Detractor" = det,
+              "Passive" = pas,
+              "Promoter" = pro,
+              "Missing value" = NA_real_)
+    
+    haven::labelled(out_x,labz)
+  }
 }
