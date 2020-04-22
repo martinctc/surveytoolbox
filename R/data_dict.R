82ec3c937e14dd3deb0e0233cd4c1e76fad9a383
Martin Chan <martinchan53@gmail.com>
2020-04-22 15:30:11 +0100

    Fix #1

--- R/data_dict.R
+++ R/data_dict.R
@@ -6,26 +6,37 @@
 #' @param x A data frame object to be passed through.
 #' @param max_char Maximum character allowed on Variable and Value label columns.       
 #' @export              
-data_dict <-function(x,max_char=32760){
+data_dict <-function(x, max_char=32760){
   suppressWarnings(
     lapply(1:ncol(x),
            function(i){
-             namex <- names(x)[[i]]
-             purrr::pluck(x,namex,purrr::attr_getter("label")) %>% as.character() -> label_var
-             purrr::pluck(x,namex,purrr::attr_getter("labels")) %>%
+             namex <- names(x)[[i]] # Extract name of each column
+             
+             label_var <-
+               purrr::pluck(x,namex,purrr::attr_getter("label")) %>%
+               as.character()
+             
+             label_val <-
+               purrr::pluck(x,namex,purrr::attr_getter("labels")) %>%
                names() %>%
                paste(collapse="; ") %>%             
-               stringr::str_trunc(max_char)-> label_val
+               stringr::str_trunc(max_char)
+             
+             val_val <-
              x[[namex]] %>%
                unique() %>%
+               sort() %>% # Fix random order bug
                paste(collapse="; ") %>%
-               stringr::str_trunc(max_char)-> val_val
+               stringr::str_trunc(max_char)
              
              
              ifelse(is_null(label_var),"",label_var)->label_var
              ifelse(is_null(label_val),"",label_val)->label_val
              
-             data.frame(var=namex,label_var,label_val,value=val_val)
+             data.frame(var=namex,
+                        label_var,
+                        label_val,
+                        value=val_val)
            }) %>% dplyr::bind_rows()
   )
 }
